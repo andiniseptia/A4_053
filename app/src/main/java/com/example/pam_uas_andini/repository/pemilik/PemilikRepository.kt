06b93379a5a8c8307a01d69b6b485152999b022b
andiniseptia <andiniseptiandri124@gmail.com>
2025-01-25 16:13:41 +0700

    Mengimplementasikan repository pemilik di network

--- app/src/main/java/com/example/pam_uas_andini/repository/pemilik/PemilikRepository.kt
+++ app/src/main/java/com/example/pam_uas_andini/repository/pemilik/PemilikRepository.kt
@@ -13,3 +13,39 @@ interface PemilikRepository {
     suspend fun deletePemilik(id_pemilik: String)
     suspend fun getPemilikById(id_pemilik: String): PemilikDetailResponse
 }
+
+class NetworkPemilikRepository(
+    private val pemilikApiService: PemilikService
+) : PemilikRepository {
+
+    override suspend fun insertPemilik(pemilik: Pemilik) {
+        pemilikApiService.insertPemilik(pemilik)
+    }
+
+    override suspend fun updatePemilik(id_pemilik: String, pemilik: Pemilik) {
+        pemilikApiService.updatePemilik(id_pemilik, pemilik)
+    }
+
+    override suspend fun deletePemilik(id_pemilik: String) {
+        try {
+            val response = pemilikApiService.deletePemilik(id_pemilik)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete pemilik. HTTP Status Code : "+
+                "${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e:Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getPemilik(): PemilikResponse {
+        return pemilikApiService.getPemilik()
+    }
+
+    override suspend fun getPemilikById(id_pemilik: String): PemilikDetailResponse {
+        return pemilikApiService.getPemilikById(id_pemilik)
+    }
+}
