a42760b67c5d2f7a40d5ef185be73f8c7eda8fe0
andiniseptia <andiniseptiandri124@gmail.com>
2025-01-25 16:54:02 +0700

    Implementasi HomeManajerViewModel

---
+++ app/src/main/java/com/example/pam_uas_andini/ui/viewmodel/manajer/HomeManajerViewModel.kt
@@ -0,0 +1,25 @@
+package com.example.pam_uas_andini.ui.viewmodel.manajer
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
+import com.example.pam_uas_andini.model.Manajer
+import com.example.pam_uas_andini.repository.manajer.ManajerRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed class HomeManajerUiState {
+    data class Success(val manajer: List<Manajer>) : HomeManajerUiState()
+    object Error: HomeManajerUiState()
+    object Loading: HomeManajerUiState()
+}
+
+class HomeManajerViewModel(private val mnj: ManajerRepository) : ViewModel() {
+    var manajerUIState: HomeManajerUiState by mutableStateOf(HomeManajerUiState.Loading)
+        private set
+
+
+}
