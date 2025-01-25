b35bd752900d7e71b9930a00410639ed6b934eb7
andiniseptia <andiniseptiandri124@gmail.com>
2025-01-25 16:12:32 +0700

    Mengimplementasikan repository manajer di network

--- app/src/main/java/com/example/pam_uas_andini/repository/manajer/ManajerRepository.kt
+++ app/src/main/java/com/example/pam_uas_andini/repository/manajer/ManajerRepository.kt
@@ -13,3 +13,40 @@ interface ManajerRepository {
     suspend fun deleteManajer(id_manajer: String)
     suspend fun getManajerById(id_manajer: String): ManajerDetailResponse
 }
+
+class NetworkManajerRepository(
+    private val manajerApiService: ManajerService
+) : ManajerRepository {
+
+    override suspend fun insertManajer(manajer: Manajer) {
+        manajerApiService.insertManajer(manajer)
+    }
+
+    override suspend fun updateManajer(id_manajer: String, manajer: Manajer) {
+        manajerApiService.updateManajer(id_manajer, manajer)
+    }
+
+    override suspend fun deleteManajer(id_manajer: String) {
+        try {
+            val response = manajerApiService.deleteManajer(id_manajer)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete manajer. HTTP Status Code : " +
+                "${response.code()}")
+            } else  {
+                response.message()
+                println(response.message())
+            }
+        } catch (e:Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getManajer(): ManajerResponse {
+        return manajerApiService.getManajer()
+    }
+
+    override suspend fun getManajerById(id_manajer: String): ManajerDetailResponse {
+        return manajerApiService.getManajerById(id_manajer)
+    }
+
+}
