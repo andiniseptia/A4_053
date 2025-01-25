7d76fe2a12708704cfd503a563481ca9a3743c44
andiniseptia <andiniseptiandri124@gmail.com>
2025-01-25 16:45:06 +0700

    Menambahkan objek destinasi untuk pemilik

--- app/src/main/java/com/example/pam_uas_andini/navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/pam_uas_andini/navigation/DestinasiNavigasi.kt
@@ -10,3 +10,32 @@ object DestinasiHome : DestinasiNavigasi {
     override val route = "home"
     override val titleRes: String = "Home"
 }
+
+// PEMILIK
+object DestinasiHomePemilik : DestinasiNavigasi {
+    override val route = "homePemilik"
+    override val titleRes: String = "Home Pemilik"
+    const val IDPEMILIK = "id"
+    val routesWithArg = "$route/{$IDPEMILIK}"
+}
+
+object DestinasiInsertPemilik : DestinasiNavigasi {
+    override val route = "insertPemilik"
+    override val titleRes: String = "Tambah Pemilik"
+    const val IDPEMILIK = "id"
+    val routesWithArg = "$route/{$IDPEMILIK}"
+}
+
+object DestinasiDetailPemilik : DestinasiNavigasi {
+    override val route = "detailPemilik"
+    override val titleRes = "Detail Pemilik"
+    const val IDPEMILIK = "id_pemilik"
+    val routesWithArg = "$route/{$IDPEMILIK}"
+}
+
+object DestinasiUpdatePemilik : DestinasiNavigasi {
+    override val route = "updatePemilik"
+    override val titleRes = "Update Pemilik"
+    const val IDPEMILIK = "id_pemilik"
+    val routesWithArg = "$route/{$IDPEMILIK}"
+}
