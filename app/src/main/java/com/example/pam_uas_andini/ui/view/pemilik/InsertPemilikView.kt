6ce7479eb422760f9c4efe79c1f0b927a7eab5d7
andiniseptia <andiniseptiandri124@gmail.com>
2025-01-25 16:35:19 +0700

    Menambahkan form input pemilik

---
+++ app/src/main/java/com/example/pam_uas_andini/ui/view/pemilik/InsertPemilikView.kt
@@ -0,0 +1,69 @@
+package com.example.pam_uas_andini.ui.view.pemilik
+
+import com.example.pam_uas_andini.ui.viewmodel.pemilik.PemilikUiEvent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pam_uas_andini.customwidget.CostumeTopAppBar
+import com.example.pam_uas_andini.navigation.DestinasiInsertPemilik
+import com.example.pam_uas_andini.ui.viewmodel.PenyediaViewModel
+import com.example.pam_uas_andini.ui.viewmodel.pemilik.InsertPemilikViewModel
+import com.example.pam_uas_andini.ui.viewmodel.pemilik.PemilikUiState
+import kotlinx.coroutines.launch
+
+
+@Composable
+fun FormPemilik(
+    pemilikUiEvent: PemilikUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (PemilikUiEvent) -> Unit = {},
+    enabled: Boolean = true
+) {
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        OutlinedTextField(
+            value = pemilikUiEvent.id_pemilik,
+            onValueChange = { onValueChange(pemilikUiEvent.copy(id_pemilik = it)) },
+            label = { Text("ID Pemilik") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+        OutlinedTextField(
+            value = pemilikUiEvent.nama_pemilik,
+            onValueChange = { onValueChange(pemilikUiEvent.copy(nama_pemilik = it)) },
+            label = { Text("Nama Pemilik") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+        OutlinedTextField(
+            value = pemilikUiEvent.kontak_pemilik,
+            onValueChange = { onValueChange(pemilikUiEvent.copy(kontak_pemilik = it)) },
+            label = { Text("Kontak Pemilik") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+    }
+}
