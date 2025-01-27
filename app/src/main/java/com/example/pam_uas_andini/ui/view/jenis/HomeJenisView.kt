84b253304e4fdfad2512d281fa629a8dda9fbc68
andiniseptia <andiniseptiandri124@gmail.com>
2025-01-27 19:40:29 +0700

    Menambahkan card untuk daftar jenis

---
+++ app/src/main/java/com/example/pam_uas_andini/ui/view/jenis/HomeJenisView.kt
@@ -0,0 +1,115 @@
+package com.example.pam_uas_andini.ui.view.jenis
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.example.pam_uas_andini.R
+import com.example.pam_uas_andini.customwidget.CostumeTopAppBar
+import com.example.pam_uas_andini.model.Jenis
+import com.example.pam_uas_andini.navigation.DestinasiHomeJenis
+import com.example.pam_uas_andini.ui.viewmodel.PenyediaViewModel
+import com.example.pam_uas_andini.ui.viewmodel.jenis.HomeJenisUiState
+import com.example.pam_uas_andini.ui.viewmodel.jenis.HomeJenisViewModel
+
+@Composable
+fun JnsCard(
+    jenis: Jenis,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (Jenis) -> Unit = {},
+    onEditClick: (Jenis) -> Unit = {}
+) {
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = colorResource(id = R.color.cardhome)
+        )
+    ) {
+        Column (
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(imageVector = Icons.Filled.Star, contentDescription = "nama", tint = Color.White)
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = jenis.nama_jenis,
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.weight(1f),
+                    color = Color.White
+                )
+
+                IconButton(onClick = { onEditClick(jenis) }) {
+                    Icon(
+                        imageVector = Icons.Default.Edit,
+                        contentDescription = "Edit",
+                        tint = Color.White
+                    )
+                }
+
+                IconButton(onClick = { onDeleteClick(jenis) }) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = null,
+                        tint = Color.White
+                    )
+                }
+            }
+
+            Row() {
+                Icon(imageVector = Icons.Filled.Info, contentDescription = "nama", tint = Color.White)
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = jenis.id_jenis,
+                    style = MaterialTheme.typography.titleMedium,
+                    color = Color.White
+                )
+            }
+        }
+    }
+}
