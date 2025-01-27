c9265456b83d28ebaa2033918bf7edd1083ff4ec
andiniseptia <andiniseptiandri124@gmail.com>
2025-01-27 19:24:22 +0700

    Mengubah warna top app bar

--- app/src/main/java/com/example/pam_uas_andini/customwidget/TopAppBar.kt
+++ app/src/main/java/com/example/pam_uas_andini/customwidget/TopAppBar.kt
@@ -1,5 +1,6 @@
 package com.example.pam_uas_andini.customwidget
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -9,9 +10,14 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import com.example.pam_uas_andini.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -27,17 +33,21 @@ fun CostumeTopAppBar(
     CenterAlignedTopAppBar(
         title = { Text(title) },
         actions = {
-            Icon(imageVector = Icons.Default.Refresh, contentDescription = "", modifier = Modifier.clickable {
+            Icon(imageVector = Icons.Default.Refresh, contentDescription = "", tint = Color.White, modifier = Modifier.clickable {
                 onRefresh()
             })
         },
-        modifier = modifier,
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = colorResource(id = R.color.primary),
+            titleContentColor = Color.White
+        ),
+        modifier = modifier.background(colorResource(id = R.color.primary)),
         scrollBehavior = scrollBehavior, navigationIcon = {
             if (canNavigateBack) {
                 IconButton(onClick = navigateUp) {
-                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
+                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null, tint = Color.White)
                 }
             }
         }
-    )
+        )
 }
